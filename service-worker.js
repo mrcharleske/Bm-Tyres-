bd58387f953fb2bd6b91ac8f544b4d7ee82aff53
mrcharleske <njorogecharles5460@gmail.com>
2025-10-17 16:39:35 +0300

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'bm-tyres-cache-v1'
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/aos@2.3.1/dist/aos.css',
+  'https://unpkg.com/aos@2.3.1/dist/aos.js'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    }).catch(()=>fetch(event.request))
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  )
+})
